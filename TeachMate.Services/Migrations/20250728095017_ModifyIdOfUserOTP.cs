b950d30b68bd99a49abb2219320409e343b2213c
huy.nguyen <ngnhathuy1224@gmail.com>
2025-07-28 16:50:30 +0700

    ModifyIdOfUserOTP

---
+++ TeachMate.Services/Migrations/20250728095017_ModifyIdOfUserOTP.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachMate.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyIdOfUserOTP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "UserOTPs",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserOTPs",
+                newName: "id");
+        }
+    }
+}
